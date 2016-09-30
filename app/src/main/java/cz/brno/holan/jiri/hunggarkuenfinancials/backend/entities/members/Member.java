67dbdbca23af3f68397a815cec6477ad9869e95e
Martin Hatina <mhatina@redhat.com>
2016-09-30 12:05:29 +0200

    license added to files

--- app/src/main/java/cz/brno/holan/jiri/hunggarkuenfinancials/backend/entities/members/Member.java
+++ app/src/main/java/cz/brno/holan/jiri/hunggarkuenfinancials/backend/entities/members/Member.java
@@ -1,12 +1,28 @@
+/*
+ * Copyright (C) 2016  Martin Hatina
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.members;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 import cz.brno.holan.jiri.hunggarkuenfinancials.R;
-import cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.contacts.Address;
-import cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.contacts.Contact;
 import cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.Payment;
+import cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.contacts.Address;
 import cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.contacts.Mail;
 import cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.contacts.Phone;
 import cz.brno.holan.jiri.hunggarkuenfinancials.backend.entities.enums.MemberStatus;
