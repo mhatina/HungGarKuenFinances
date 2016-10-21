3c4d8536a88522b21c452a854f44292e5a38e119
Martin Hatina <mhatina@redhat.com>
2016-10-21 11:42:25 +0200

    ellipsize contact

--- app/src/main/java/cz/brno/holan/jiri/hunggarkuenfinancials/Constant.java
+++ app/src/main/java/cz/brno/holan/jiri/hunggarkuenfinancials/Constant.java
@@ -2,8 +2,8 @@ package cz.brno.holan.jiri.hunggarkuenfinancials;
 
 
 public class Constant {
-    public static final int SIGN_IN_CODE = 1;
-    public static final int FILE_SELECT_CODE = 2;
+    public static final int SIGN_IN_CODE = 100;
+    public static final int FILE_SELECT_CODE = 101;
 
     public static final String NAME_REGEX = "[A-Z][a-zA-Z ]+";
     public static final String PHONE_REGEX = "^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$";
